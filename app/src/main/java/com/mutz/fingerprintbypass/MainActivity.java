641089cf22787ccfdfbc9a8e7d9e64299b44f9f0
mutz555 <mrbosscoc22@gmail.com>
2025-05-04 06:42:23 +0700

    Update MainActivity.java

--- app/src/main/java/com/mutz/fingerprintbypass/MainActivity.java
+++ app/src/main/java/com/mutz/fingerprintbypass/MainActivity.java
@@ -1,26 +1,19 @@
 package com.mutz.fingerprintbypass;
 
-import android.app.Application;
 import android.util.Log;
 
-import com.highcapable.yukihookapi.hook.factory.HookFactory;
 import com.highcapable.yukihookapi.hook.factory.YukiHookHelper;
-import com.highcapable.yukihookapi.hook.type.HookParam;
 import com.highcapable.yukihookapi.template.YukiHookModule;
 
 public class MainActivity extends YukiHookModule {
-
     @Override
     public void onHook() {
-        YukiHookHelper.encase(config -> {
-            config.name("android"); // Target sistem server
-            config.onHook(() -> {
-                config.hookClass("com.android.server.biometrics.sensors.fingerprint.FingerprintServiceStubImpl", hook -> {
-                    hook.method("isFpHardwareDetected")
-                        .replaceToTrue(param -> {
-                            Log.i("FingerprintBypass", "isFpHardwareDetected() telah dibypass ke true");
-                        });
-                });
+        onApp("android", hook -> {
+            hook.hookClass("com.android.server.biometrics.sensors.fingerprint.FingerprintServiceStubImpl", cls -> {
+                cls.method("isFpHardwareDetected")
+                   .replaceToTrue(param -> {
+                       Log.i("FingerprintBypass", "isFpHardwareDetected() dibypass ke true");
+                   });
             });
         });
     }
