52745b0bcd3234bd0c8b1e02c8327521c747b72a
mutz555 <mrbosscoc22@gmail.com>
2025-05-16 21:19:18 +0700

    Update HookEntry.kt

--- app/src/main/kotlin/com/mutz/fingerprintbypass/HookEntry.kt
+++ app/src/main/kotlin/com/mutz/fingerprintbypass/HookEntry.kt
@@ -6,7 +6,7 @@ import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
-import android.hardware.biometrics.fingerprint.SensorProps // Import SensorProps
+import android.hardware.biometrics.fingerprint.SensorProps // Pastikan ini benar!
 
 class HookEntry : IXposedHookLoadPackage {
 
@@ -15,6 +15,7 @@ class HookEntry : IXposedHookLoadPackage {
     }
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        XposedBridge.log("$TAG: handleLoadPackage: ${lpparam.packageName}")
         if (lpparam.packageName == "android") {
             hookGetSensorPropForInstance(lpparam)
         }
@@ -22,10 +23,14 @@ class HookEntry : IXposedHookLoadPackage {
 
     private fun hookGetSensorPropForInstance(lpparam: XC_LoadPackage.LoadPackageParam) {
         try {
+            XposedBridge.log("$TAG: hookGetSensorPropForInstance called")
+            XposedBridge.log("$TAG: ClassLoader: ${lpparam.classLoader}")
+
             val clazz = XposedHelpers.findClass(
                 "android.hardware.fingerprint.FingerprintSensorConfigurations",
                 lpparam.classLoader
             )
+            XposedBridge.log("$TAG: FingerprintSensorConfigurations class found: $clazz")
 
             XposedHelpers.findAndHookMethod(
                 clazz,
@@ -40,8 +45,14 @@ class HookEntry : IXposedHookLoadPackage {
                     override fun afterHookedMethod(param: MethodHookParam) {
                         val instance = param.args[0] as? String
                         val result = param.result
+                        XposedBridge.log("$TAG: getSensorPropForInstance result: $result")
+
                         if (result is Array<*>) {
-                            val props = result as Array<android.hardware.biometrics.fingerprint.SensorProps> // Specify type here
+                            @Suppress("UNCHECKED_CAST") // Tambahkan ini
+                            val props = result as Array<android.hardware.biometrics.fingerprint.SensorProps>
+
+                            XposedBridge.log("$TAG: props class: ${props.javaClass.name}") // Tambahkan ini
+                            XposedBridge.log("$TAG: props size: ${props.size}")
 
                             Log.d(TAG, "getSensorPropForInstance() returned ${props.size} props for instance: $instance")
                             props.forEachIndexed { index, prop ->
@@ -93,6 +104,7 @@ class HookEntry : IXposedHookLoadPackage {
 
         } catch (e: Throwable) {
             XposedBridge.log("$TAG: getSensorPropForInstance hook failed: ${e.message}")
+            XposedBridge.log("$TAG: Error: $e")
         }
     }
 }
